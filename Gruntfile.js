e9dc980cd5a0f1613fe7b3c26daeac5d5e0332ef
R. Tyler Julian <trjulian@gmail.com>
2015-04-01 15:40:46 -0700

    Create Gruntfile.js
    Added Grunt and added its dependencies to package.json

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'server/**/*.js', 'index.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [
+    'jshint',
+    'watch'
+    ]);
+
+};
