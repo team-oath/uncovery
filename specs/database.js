98afa3d3d39ef11afc50bff973fb2ea2dd5113c8
R. Tyler Julian <trjulian@gmail.com>
2015-04-02 12:24:26 -0700

    Add test for database connection

--- specs/database.js
+++ specs/database.js
@@ -1,9 +1,13 @@
 var chai = require('chai');
 var expect = chai.expect;
+var db = require('../server/db/config');
 
 describe('database storage', function(){
-  it('should open a database connection', function() {
-    expect(true).to.equal(false);
+  it('should open a database connection', function(done) {
+    db.initialize(function(err) {
+      expect(err).to.equal(null);
+      done();
+    });
   });
 
 });
