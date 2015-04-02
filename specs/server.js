12a00ea1ac0ba370e5c2bf41c34922e82522eca8
Anthony Zotti <azotti1337@gmail.com>
2015-04-01 18:56:52 -0700

    Include request module to test suite

--- specs/server.js
+++ specs/server.js
@@ -1,7 +1,13 @@
 var chai = require(['chai']);
 var expect = chai.expect;
+var request = require(['request']);
+
 describe('server', function(){ 
   it('should fail', function() {
-    expect(true).to.be.false;
+    request('localhost:9876', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body) // Show the HTML for the Google homepage.
+        }
+    });
   });
 });
