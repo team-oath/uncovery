211d27908e32f681eedaaa229ecf78b3500f6e68
R. Tyler Julian <trjulian@gmail.com>
2015-04-02 16:34:44 -0700

    Add comments to describe how the db insertion function behaves

--- server/db/models.js
+++ server/db/models.js
@@ -9,22 +9,26 @@ exports.insert = function(userData, callback) {
   // Validate that correctly formatted userData that has been passed to `insert`
   if (validateInput(userData)) {
 
-    // Use the provided userData to create custom objects to populate our
-    // two separate tables, `marks` and `messages`.
+    // Use the provided userData to create the custom `message` to insert
     var message = createMessage(userData);
 
-    // Create an INSERT query to add the `mark` to the database
+    // Create an INSERT query to add the `message` to the database
     db.connection.query('INSERT INTO messages SET ?', message, function(err, msg) {
       if (err) callback(err);
 
+      // Use the provided userData to create the custom `mark` to insert
       var mark = createMark(userData, msg.insertId);
 
+      // Create an INSERT query to add the `mark` to the database
       db.connection.query('INSERT INTO marks SET ?', mark, function(err, mark) {
         if (err) callback(err);
+
+        // Let the function callee know that the insertion was successful
         callback('Successfully inserted new message and mark to database.');
       });
     });
   } else {
+    // Let the function callee know that the insertion was unsuccessful
     callback('Could not insert new message: invalid input.')
   }
 };
@@ -38,6 +42,8 @@ var validateInput = function(userData) {
   return true;
 }
 
+// Create a mark to add to the database. This function essentially
+// parses the userData to extract only the coordinates and message foreign key
 var createMark = function(userData, messageId) {
   var mark = {
     x: userData.x,
@@ -48,6 +54,8 @@ var createMark = function(userData, messageId) {
   return mark;
 };
 
+// Create a message to add to the database. For now, this only includes strings
+// In the future, this function will be able to accept many content types
 var createMessage = function(userData) {
   var message = {
     messageString: userData.message
