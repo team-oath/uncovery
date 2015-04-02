b79ede0cd625b21e56d1153cac4824244e62c79d
Anthony Zotti <azotti1337@gmail.com>
2015-04-01 19:31:35 -0700

    Create and pass specs for server http request

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Helloasdf!');
+  res.send('works');
+});
+
+app.post('/', function(req, res) {
+  res.status(201);
+  res.send('works');
 });
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
--- specs/server.js
+++ specs/server.js
@@ -1,13 +1,28 @@
-var chai = require(['chai']);
+var chai = require('chai');
 var expect = chai.expect;
-var request = require(['request']);
+var request = require('request');
+var localServerUri = 'http://127.0.0.1:3000/';
+var invalidUri = 'http://127.0.0.1:3000/invalid';
 
-describe('server', function(){ 
-  it('should fail', function() {
-    request('localhost:9876', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Show the HTML for the Google homepage.
-        }
+describe('server http routing', function(){
+  it('should receive status code 200 on GET request', function(done) {
+    request(localServerUri, function (err, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should receive status code 201 on POST request', function(done) {
+    request.post({url:localServerUri, form: {key:'value'}}, function(err, response, body) { 
+      expect(response.statusCode).to.equal(201);
+      done();
+    });
+  });
+
+  it('should receive status code 404 on GET request to an invalid endpoint', function(done) {
+    request(invalidUri, function(err, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
     });
   });
 });
