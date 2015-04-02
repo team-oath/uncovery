e2b2798523af5ff6f8232c6ce60190d53f9a6a6d
R. Tyler Julian <trjulian@gmail.com>
2015-04-02 14:34:04 -0700

    Create mark and message tables in database

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,22 @@
 CREATE DATABASE uncovery;
 
 USE uncovery;
+
+CREATE TABLE mark (
+  mark_id int(5) AUTO_INCREMENT,
+  loc_x float(10, 6),
+  loc_y float(10, 6),
+  loc_z float(10, 6),
+  timestamp timestamp,
+  UNIQUE (
+    mark_id
+  )
+);
+
+CREATE TABLE message (
+  message_id int(5) AUTO_INCREMENT,
+  message_string text,
+  UNIQUE (
+    message_id
+  )
+);
