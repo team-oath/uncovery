dff19361ecd3da7a48b64b9262652eb5f2a05a1e
R. Tyler Julian <trjulian@gmail.com>
2015-04-02 12:11:53 -0700

    Create initial spec file for database tests

---
+++ specs/database.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('database storage', function(){
+  it('should open a database connection', function() {
+    expect(true).to.equal(false);
+  });
+
+});
