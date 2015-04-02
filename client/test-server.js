aa9e2a09ff202701ffbac9f1b6553e4988582fe3
Jonathan Huang <grokut@gmail.com>
2015-04-02 15:28:17 -0700

    added mock server for testing client

---
+++ client/test-server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var MOCK_DATA = require('./mockData.js')
+
+server.listen(6666, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('app listening at http://%s:%s', host, port);
+});
+
+console.log(MOCK_DATA);
