7c9c947976d71099a6176f81662843a7d8e7f64e
scottdixon <scott@howrefreshing.com.au>
2015-04-02 11:59:49 -0700

    Abstracted components

---
+++ client/styles.js
@@ -0,0 +1,19 @@
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  listView: {
+    paddingTop: 0,
+    paddingRight: 10,
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+module.exports = styles;
