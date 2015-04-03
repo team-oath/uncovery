18dbfb541960465944971269a393af668ecbb33d
R. Tyler Julian <trjulian@gmail.com>
2015-04-02 17:28:51 -0700

    Add tests to confirm message retrieval

--- specs/database.js
+++ specs/database.js
@@ -15,6 +15,10 @@ describe('database storage', function() {
     y: -122.408974,
     z: 69
   };
+  var testLocation = {
+    x: 37.183549,
+    y: -122.108974,
+  };
 
   it('should validate user input', function(done) {
     models.insert(invalidTestData, function(msg) {
@@ -26,6 +30,13 @@ describe('database storage', function() {
   it('should add messages to the database', function(done) {
     models.insert(testData, function(msg) {
       expect(msg).to.equal('Successfully inserted new message and mark to database.');
+      done();
+    })
+  });
+
+  it('should retrieve messages from the database', function(done) {
+    models.retrieve(testLocation, function(messages) {
+      expect(messages).to.be.instanceof(Array);
       done();
     })
   });
