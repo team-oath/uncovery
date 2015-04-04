e241eaa706f59d7ae517f6a3b75aead32d0f39a7
Jonathan Huang <grokut@gmail.com>
2015-04-04 09:42:03 -0700

    style/ refactored Message Element to separate file

---
+++ client/components/Message.js
@@ -0,0 +1,117 @@
+
+var React = require('react-native');
+
+var {View, Text, TouchableOpacity, LayoutAnimation, StyleSheet} = React;
+
+var Message = React.createClass({
+  getInitialState: function() {
+    return {dir: 'row'};
+  },
+  _onPressMessage: function() {
+    var config = layoutAnimationConfigs[0];
+    console.log(config)
+    LayoutAnimation.configureNext(config);
+    this.setState({
+      dir: this.state.dir === 'row' ? 'column' : 'row',
+    });
+  },
+
+  render: function(body) {
+    return (
+
+    <TouchableOpacity onPress={() => this._onPressMessage()}>
+      {this.state.dir === 'column' ?
+        <View style={[styles.buttonContents, {flexDirection: this.state.dir}]}>
+          <Text> </Text>
+          <Text style={{fontSize: 8}}> {this.props.body.timestamp} @ {this.props.body.distance} </Text>
+          <Text> {this.props.body.messageString} </Text>
+        </View> :
+        <View style={[styles.buttonContents, {flexDirection: this.state.dir}]}>
+          <Text> </Text>
+          <Text style={{fontSize: 8}}> {this.props.body.timestamp} @ {this.props.body.distance} </Text>
+          <Text> {this.props.body.messageString.substring(0,10)} </Text>
+        </View>
+      }
+     </TouchableOpacity>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  header: {
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3B5998',
+    flexDirection: 'row',
+  },
+  text: {
+    color: 'white',
+    paddingHorizontal: 8,
+  },
+  rowText: {
+    color: '#888888',
+  },
+  thumbText: {
+    fontSize: 20,
+    color: '#888888',
+  },
+  buttonContents: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    marginVertical: 3,
+    padding: 5,
+    backgroundColor: '#EAEAEA',
+    borderRadius: 3,
+    paddingVertical: 10,
+  },
+  img: {
+    width: 64,
+    height: 64,
+    marginHorizontal: 10,
+  },
+  section: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: 6,
+    backgroundColor: '#5890ff',
+  },
+});
+
+var animations = {
+  layout: {
+    spring: {
+      duration: 750,
+      create: {
+        duration: 300,
+        type: LayoutAnimation.Types.easeInEaseOut,
+        property: LayoutAnimation.Properties.opacity,
+      },
+      update: {
+        type: LayoutAnimation.Types.spring,
+        springDamping: 0.4,
+      },
+    },
+    easeInEaseOut: {
+      duration: 300,
+      create: {
+        type: LayoutAnimation.Types.easeInEaseOut,
+        property: LayoutAnimation.Properties.scaleXY,
+      },
+      update: {
+        delay: 100,
+        type: LayoutAnimation.Types.easeInEaseOut,
+      },
+    },
+  },
+};
+
+var layoutAnimationConfigs = [
+  animations.layout.spring,
+  animations.layout.easeInEaseOut,
+];
+
+module.exports = Message
--- client/components/marks.js
+++ client/components/marks.js
@@ -1,42 +1,9 @@
+
 var React = require('react-native');
-// var styles = require("../styles.js");
+var Message = require('./Message.js')
+var config = require('../config.js')
 
-
-var {View, ListView, Text, TouchableOpacity, LayoutAnimation, StyleSheet} = React;
-
-var Message = React.createClass({
-  getInitialState: function() {
-    return {dir: 'row'};
-  },
-  _onPressMessage: function() {
-    var config = layoutAnimationConfigs[0];
-    console.log(config)
-    LayoutAnimation.configureNext(config);
-    this.setState({
-      dir: this.state.dir === 'row' ? 'column' : 'row',
-    });
-  },
-
-  render: function(body) {
-    return (
-
-    <TouchableOpacity onPress={() => this._onPressMessage()}>
-      {this.state.dir === 'column' ?
-        <View style={[styles.buttonContents, {flexDirection: this.state.dir}]}>
-          <Text> </Text>
-          <Text style={{fontSize: 8}}> {this.props.body.timestamp} @ {this.props.body.distance} </Text>
-          <Text> {this.props.body.messageString} </Text>
-        </View> :
-        <View style={[styles.buttonContents, {flexDirection: this.state.dir}]}>
-          <Text> </Text>
-          <Text style={{fontSize: 8}}> {this.props.body.timestamp} @ {this.props.body.distance} </Text>
-          <Text> {this.props.body.messageString.substring(0,10)} </Text>
-        </View>
-      }
-     </TouchableOpacity>
-    );
-  }
-});
+var {View, ListView, Text} = React;
 
 class Marks extends React.Component {
 
@@ -55,7 +22,7 @@ class Marks extends React.Component {
         }
       } 
     };
-   }
+  }
 
   componentDidMount() {
    
@@ -86,7 +53,9 @@ class Marks extends React.Component {
     var x = this.state.lastPosition.coords.latitude;
     var y = this.state.lastPosition.coords.longitude;
     var z = this.state.lastPosition.coords.altitude;
-    var requestURL = 'http://uncovery.ngrok.com/?'+'x='+x+'&'+'y='+y+'&'+'z='+z
+    var requestURL = config.host + 'x='+x+'&'+'y='+y+'&'+'z='+z
+
+    console.log('The Request URL is ', requestURL);
 
     fetch(requestURL)
       .then((response) => response.json())
@@ -118,11 +87,10 @@ class Marks extends React.Component {
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderMessage}
-        style={styles.listView}
+        style={{backgroundColor: '#B0C4DE'}}
         initialListSize={10}
         pageSize={4}
         scrollRenderAheadDistance={2000} />
-
     );
   }
 
@@ -133,86 +101,5 @@ class Marks extends React.Component {
   }
  
 };
-
-var styles = StyleSheet.create({
-  listView: {
-    backgroundColor: '#B0C4DE',
-  },
-  header: {
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#3B5998',
-    flexDirection: 'row',
-  },
-  text: {
-    color: 'white',
-    paddingHorizontal: 8,
-  },
-  rowText: {
-    color: '#888888',
-  },
-  thumbText: {
-    fontSize: 20,
-    color: '#888888',
-  },
-  buttonContents: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 5,
-    marginVertical: 3,
-    padding: 5,
-    backgroundColor: '#EAEAEA',
-    borderRadius: 3,
-    paddingVertical: 10,
-  },
-  img: {
-    width: 64,
-    height: 64,
-    marginHorizontal: 10,
-  },
-  section: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    padding: 6,
-    backgroundColor: '#5890ff',
-  },
-});
-
-var animations = {
-  layout: {
-    spring: {
-      duration: 750,
-      create: {
-        duration: 300,
-        type: LayoutAnimation.Types.easeInEaseOut,
-        property: LayoutAnimation.Properties.opacity,
-      },
-      update: {
-        type: LayoutAnimation.Types.spring,
-        springDamping: 0.4,
-      },
-    },
-    easeInEaseOut: {
-      duration: 300,
-      create: {
-        type: LayoutAnimation.Types.easeInEaseOut,
-        property: LayoutAnimation.Properties.scaleXY,
-      },
-      update: {
-        delay: 100,
-        type: LayoutAnimation.Types.easeInEaseOut,
-      },
-    },
-  },
-};
-
-var layoutAnimationConfigs = [
-  animations.layout.spring,
-  animations.layout.easeInEaseOut,
-];
-
 
 module.exports = Marks;
