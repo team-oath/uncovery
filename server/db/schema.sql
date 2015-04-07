dabcc1dc213e2fc964334552ce6684f31c6cd69c
Anthony Zotti <azotti1337@gmail.com>
2015-04-07 12:10:47 -0700

    Update MySQL Schema for ranking system

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE uncovery;
 CREATE DATABASE uncovery;
 
 SET foreign_key_checks=0;
@@ -6,18 +7,34 @@ USE uncovery;
 
 CREATE TABLE marks (
   id int(5) AUTO_INCREMENT,
+  PRIMARY KEY (id),
   x float(10, 6),
   y float(10, 6),
   z float(10, 6),
   timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
   messageId int(5),
-  FOREIGN KEY (messageId)
-    REFERENCES messages(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY (messageId) REFERENCES messages(id),
+  userId int(5),
+  FOREIGN KEY (userId) REFERENCES users(id)
 );
 
 CREATE TABLE messages (
   id int(5) AUTO_INCREMENT,
+  PRIMARY KEY (id),
   messageString text,
-  PRIMARY KEY (id)
+  score int(5) DEFAULT 0
+);
+
+CREATE TABLE votes (
+  id int(5) AUTO_INCREMENT, 
+  PRIMARY KEY(id),
+  userId int(5),
+  FOREIGN KEY (userId) REFERENCES users(id),
+  messageId int(5),
+  FOREIGN KEY (messageId) REFERENCES messages(id)
+);
+
+CREATE TABLE users (
+  id int(5) AUTO_INCREMENT,
+  PRIMARY KEY(id)
 );
