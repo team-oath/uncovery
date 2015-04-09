554af2333a33c5809f579424b07c43ac0ef8b991
scottdixon <scott@howrefreshing.com.au>
2015-04-09 12:04:03 -0700

    Select from marks statement returns unix timestamp

--- server/db/models.js
+++ server/db/models.js
@@ -54,7 +54,7 @@ exports.retrieve = function(userLocation, callback) {
 
   if (validateQuery(userLocation)) {
     var query = ([
-      'SELECT *',
+      'SELECT *, unix_timestamp(timestamp)',
       'FROM marks',
       'LEFT JOIN messages',
       'ON marks.messageId = messages.id',
