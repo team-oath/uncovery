48800e8a8797ea26dc7bb262d16e31dde2e3f353
scottdixon <scott@howrefreshing.com.au>
2015-04-09 15:31:38 -0700

    /landing SASS Compass config.rb

---
+++ config.rb
@@ -0,0 +1,15 @@
+http_path = "/"
+css_dir = "/assets/css"
+images_dir = "/assets/img"
+fonts_dir = "/assets/fonts"
+relative_assets = true
+
+# dev
+#line_comments = true;
+
+# prod/demo
+#line_comments = false;
+#output_style = :compressed;
+
+# delivery
+line_comments = false;
