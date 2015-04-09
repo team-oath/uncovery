f3a91eaa588cdac3c10e1027f95f66620f90d7a7
scottdixon <scott@howrefreshing.com.au>
2015-04-09 14:38:03 -0700

    Initital landing page theme

---
+++ server/landing/assets/js/zi-script.js
@@ -0,0 +1,108 @@
+$(document).ready( function() {
+
+	/*----------------------/
+	/* PAGE LOADER
+	/*---------------------*/
+
+	$(window).on('load', function() {
+		$('#preloader .loader-wrapper').fadeOut('slow');
+		$('#preloader').delay(350).fadeOut('slow');
+	});
+
+
+	/*----------------------/
+	/* NAVIGATION
+	/*---------------------*/
+
+	$('#navigation').localScroll({
+		duration: 1000,
+		easing: 'easeInOutExpo'
+	});
+
+	$(window).on('scroll', function(){
+		if( $(document).scrollTop() > 150 ) {
+			$('.navbar').addClass('active');
+
+		}else {
+			$('.navbar').removeClass('active');
+		}
+	});
+	
+	$('#navigation li a').on('click', function() {
+		if($(this).parents('.navbar-collapse.collapse').hasClass('in')) {
+			$('#navigation').collapse('hide');
+		}
+	});
+
+
+	/*----------------------/
+	/* INIT FITVID
+	/*---------------------*/
+
+	$('.page-video-wrapper').fitVids();
+
+
+	/*-----------------------------/
+	/* FEATURE CONTENT SLIDER
+	/*---------------------------*/
+
+	$('.feature-slides').bxSlider({
+		mode: 'fade',
+		auto: true,
+		speed: 500,
+		controls: false,
+		adaptiveHeight: true
+	});
+
+	
+	/*-----------------------------/
+	/* INFINITE SCREENSHOT SLIDER
+	/*---------------------------*/
+
+	$('.app-screenshots').slick({
+		dots: true,
+		centerMode: true,
+		cssEase: 'ease-in',
+		variableWidth: true,
+	});
+
+
+	/*----------------------/
+	/* NAVIGATION SCROLLING
+	/*---------------------*/
+
+	$(window).scroll( function() {
+		if( $(this).scrollTop() > 300 ) {
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
+
+	$('.back-to-top').on( 'click', function(e) {
+		e.preventDefault();
+
+		$('body, html').animate({
+			scrollTop: 0
+		}, 800, 'easeInOutExpo');
+	});
+
+	if($('.learn-more').length > 0) {
+		$('.learn-more').localScroll({
+			duration: 1000,
+			easing: 'easeInOutExpo'
+		});
+	}
+
+
+	/*----------------------/
+	/* FULLSCREEN VIDEO
+	/*---------------------*/
+
+	if($(".fullscreen-video-bg").length > 0) {
+		if($.browser.mobile) {
+			$(".fullscreen-video-bg").addClass('no-video-bg');
+		}
+	}
+
+});
