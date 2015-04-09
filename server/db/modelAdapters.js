159947ffbaf1c96fcfae0a48671c170d58997d2d
Anthony Zotti <azotti1337@gmail.com>
2015-04-09 11:35:34 -0700

    Create modelAdapters for low level DB interactions

---
+++ server/db/modelAdapters.js
@@ -0,0 +1,81 @@
+var db = require('./config');
+var util = require('../core/utilities');
+
+db.initialize();
+
+// insert(string, object)
+exports.insert = function(table, data) {
+  return new Promise(function(resolve, reject) {
+    var query = 'INSERT INTO ?? SET ?';
+    var params = [table, data];
+    db.connection.query(query, params, function(err, connection) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
+
+//selectAll(string tableName)
+exports.selectAll = function(table) {
+  return new Promise(function(resolve, reject) {
+    var query = 'SELECT * FROM ??';
+    db.connection.query(query, table, function(err, fields) {
+      if (err) {
+        reject(err, 'Error in selectAll method');
+      } else {
+        resolve(fields);
+      }
+    });
+  });
+};
+
+//filters is an array of key value pairs
+//filters follows this structure:
+// -- [key1, value1, key2, value2 ...etc]
+// where(string tableName, [string key1, string value1])
+exports.where = function(table, filters) {
+  return new Promise(function(resolve, reject) {
+    var query = 'SELECT * FROM ?? WHERE ?? = ?';
+    var params = ([table]).concat(filters);
+    db.connection.query(query, params, function(err, connection) {
+      if (err) {
+        reject(err, 'Error in where method');
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
+
+//update(string tableName, [string key1, string value1, string key2, string value2]);
+exports.update = function(table, filters) {
+  return new Promise(function(resolve, reject) {
+    var params = ([table]).concat(filters);
+    var query = 'UPDATE ?? SET ?? = ? WHERE ?? = ?';
+    db.connection.query(query, params, function(err, connection) {
+      if (err) {
+        reject(err, 'Error in update method');
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
+
+//delete(string tableName, [string key, string value]);
+exports.delete = function(table, filters) {
+  return new Promise(function(resolve, reject) {
+    var params = ([table]).concat(filters);
+    var query = 'DELETE FROM ?? WHERE ?? = ?';
+    db.connection.query(query, params, function(err, connection) {
+      if (err) {
+        reject(err, 'Error in delete method');
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
