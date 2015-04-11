13fe62e220452227ec19a8cf56b484a5e0a3b26b
Anthony Zotti <azotti1337@gmail.com>
2015-04-11 15:15:49 -0700

    Refactor integration specs for readability

--- specs/integration.js
+++ specs/integration.js
@@ -2,36 +2,35 @@ var chai = require('chai');
 var expect = chai.expect;
 var models = require('../server/db/models');
 var request = require('request');
+
 var localServerUri = 'http://127.0.0.1:3000/';
-var GETUri = localServerUri + 'messages/?x=100.123456&y=-50.323&z=14.4244';
-var testData = {x: 100.123456, y: -50.323, z: 14.4244, message: 'hello database!'};
+var getMessagesUri = localServerUri + 'messages/?x=100.123456&y=-50.323&z=14.4244';
 var testUser = {userToken: 'ABCDEFG'};
+var postData = {url: localServerUri + 'usertoken', form: testUser};
+
 
 describe('server to database integration', function() {
 
-  after(function() {
-    models.delete('marks', ['x', 100.123459]);
-    models.delete('messages', ['messageString', 'hello database!']);
-    models.delete('users', ['token', 'ABCDEFG']);
-  });
-
-  it('should GET messages from the database', function(done) {
-    request(GETUri, function(err, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+  after(function(done) {
+    models.deleteRow('marks', ['x', 100.123459]).then(function() {
+      models.deleteRow('messages', ['messageString', 'hello database!']).then(function() {
+        models.deleteRow('users', ['token', 'ABCDEFG']).then(function() {
+          done();
+        });
+      });
     });
   });
 
   it('should POST a new user token to the database', function(done) {
-    request.post({url:localServerUri + 'usertoken', form: testUser}, function(err, response, body) {
+    request.post(postData, function(err, response) {
       expect(response.statusCode).to.equal(201);
       done();
     });
   });
 
-  it('should persist valid POST request to database', function(done) {
-    request.post({url:localServerUri + 'messages', form: testData}, function(err, response, body) {
-      expect(response.statusCode).to.equal(201);
+  it('should GET messages from the database', function(done) {
+    request(getMessagesUri, function(err, response, body) {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
