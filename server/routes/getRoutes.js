7503f9c51fd34e9d18a82ce0e4ff1a47b749a82a
Anthony Zotti <azotti1337@gmail.com>
2015-04-11 07:50:10 -0700

    Refactor duplicate GET requests code

--- server/routes/getRoutes.js
+++ server/routes/getRoutes.js
@@ -1,32 +1,34 @@
 var models = require('../db/models.js');
 var util = require('../core/utilities.js');
+
+var reject = function(req, res, err) {
+  util.log('RECEIVED a GET request', req.query);
+  util.log('SENT error code to user', err);
+  res.sendStatus(400);
+};
+
+var resolve = function(req, res, success) {
+  util.log('RECEIVED a GET request', req.query);
+  util.log('SENT successful response to user', success);
+  res.status(200);
+  res.send(success);
+};
+
 module.exports = function(router) {
 
   //{x: float, y: float, z: float}
   router.get('/messages', function (req, res) {
-    util.log('RECEIVED a GET request', req.query);
-    models.retrieveMarks(req.query)
-      .then(function(msg) {
-        util.log('SENT message array to user', msg);
-        res.status(200);
-        res.send(msg);
-      }, function(err) {
-        util.log('SENT error code to user', err);
-        res.sendStatus(400);
-      });
+    models.retrieveMarks(req.query).then(
+          resolve.bind(this, req, res),
+          reject.bind(this, req, res)
+          );
   });
 
   //{messageId: string}
   router.get('/comment', function(req, res) {
-    util.log('RECEIVED a GET request', req.query);
-    models.retrieveComments(+req.query.messageId)
-      .then(function(comments) {
-        util.log('SENT comments to user', comments);
-        res.status(200);
-        res.send(comments);
-      }, function(err) {
-        util.log('SENT error code to user', err);
-        res.sendStatus(400);
-      });
+    models.retrieveComments(req.query.messageId).then(
+          resolve.bind(this, req, res),
+          reject.bind(this, req, res)
+          );
   });
 };
