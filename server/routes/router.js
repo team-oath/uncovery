ae6308f5c2b32bf19e5e228c4ea820e620a908bd
R. Tyler Julian <trjulian@gmail.com>
2015-04-11 15:21:07 -0700

    Add server logging with Morgan module

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var morgan = require('morgan');
 var models = require('../db/models.js');
 var util = require('../core/utilities.js');
 var router = express.Router();
 
+router.use(morgan('combined', util.getLogStream()))
 require('./getRoutes.js')(router);
 require('./postRoutes.js')(router);
 
