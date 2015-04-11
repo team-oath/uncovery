767d68570ea118a80ddadcc7199b0c2fec78a803
Anthony Zotti <azotti1337@gmail.com>
2015-04-11 15:03:44 -0700

    Implement deleteMarkByUserToken and deleteMessagesByScore

--- specs/server.js
+++ specs/server.js
@@ -2,18 +2,21 @@ var chai = require('chai');
 var models = require('../server/db/models.js');
 var expect = chai.expect;
 var request = require('request');
-var localServerUri = 'http://127.0.0.1:3000/';
+var messageUri = 'http://127.0.0.1:3000/messages';
+var upvoteUri = 'http://127.0.0.1:3000/upvote';
 var invalidUri = 'http://127.0.0.1:3000/invalid';
-var testData = {
-  x: 110.123456,
-  y: -50.323,
-  z: 14.4244,
-  message: 'hello server',
-  userToken: 'foobar'
-};
 
 describe('Node Server', function(){
-  
+
+  var testData = {
+    x: 110.123456,
+    y: -50.323,
+    z: 14.4244,
+    score: -10,
+    message: 'hello server',
+    userToken: 'foobar'
+  };
+
   before(function(done) {
     models.createUser(testData.userToken).then(function() {
       done();
@@ -21,45 +24,44 @@ describe('Node Server', function(){
   });
 
   after(function(done) {
-    models.deleteUser(testData.userToken).then(function() {
-      done();
+    models.deleteMarkByUserToken(testData.userToken).then(function() {
+      models.deleteUser(testData.userToken).then(function() {
+        models.deleteMessagesByScore(-10).then(function() {
+          done();
+        });
+      });
     });
   });
 
-  xdescribe('Basic http routing', function(){
+  describe('Basic http routing', function() {
+
     it('should receive status code 201 on POST request', function(done) {
-      request.post({url: localServerUri, form: {key:'value'}}, function(err, response, body) {
+      request.post({url: messageUri, form: testData}, function(err, response) {
         expect(response.statusCode).to.equal(201);
         done();
       });
     });
 
     it('should receive status code 200 on GET request', function(done) {
-      request(localServerUri, function (err, response, body) {
+      var GETuri = messageUri + '/?x=' + testData.x + '&y=' + testData.y + '&z=' + testData.z;
+      request(GETuri, function (err, response) {
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
 
     it('should receive status code 404 on GET request to an invalid endpoint', function(done) {
-      request(invalidUri, function(err, response, body) {
+      request(invalidUri, function(err, response) {
         expect(response.statusCode).to.equal(404);
         done();
       });
     });
   });
 
-  xdescribe('Feature-based http routing', function(){
-    it('should send a unique user token to the user', function(done) {
-      request(localServerUri + 'usertoken', function(err, response, body) {
-        var body = JSON.parse(response.body);
-        expect(body.userToken).to.not.be.an('undefined');
-        done();
-      });
-    });
+  describe('Feature-based http routing', function(){
 
     it('should confirm receipt of an upvote', function(done) {
-      request.post({url:localServerUri + 'upvote'}, function(err, response, body) {
+      request.post({url: upvoteUri}, function(err, response, body) {
         expect(response.statusCode).to.equal(201);
         done();
       });
