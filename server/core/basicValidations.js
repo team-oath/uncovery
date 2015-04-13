55803d7a6a2f7e536628be0404c29b02d638d62a
Anthony Zotti <azotti1337@gmail.com>
2015-04-13 16:52:10 -0700

    routeValidation injects predicates into eachCord

--- server/core/basicValidations.js
+++ server/core/basicValidations.js
@@ -1,35 +1,25 @@
 //basic validations are the building blocks for all other validations
 
-var eachCoord = function(userData, predicate) {
+
+exports.eachCoord = function(userData, predicate) {
   var coordinatesToValidate = ['x', 'y', 'z'];
   for (var i = 0;i < coordinatesToValidate.length;i++) {
     var coord = coordinatesToValidate[i];
-    if (predicate(userData[coord])) {
+    if (predicate(userData, userData[coord])) {
       return false;
     }
   }
   return true;
 };
 
-exports.containsOnlyNumbers = function(userData) {
-  var predicate = function(coord) {
+exports.containsOnlyNumbers = function(userData, coord) {
     return !!/[^0-9]/.test(Math.floor(coord));
-  };
-  return eachCoord(userData, predicate);
-}
-
-exports.inRange = function (userData) {
-  var predicate = function(coord) {
-    return coord === '' || +coord > 180 || +coord < 0;
-  };
-  return eachCoord(userData, predicate);
 };
 
-exports.coordinatesExist = function(userData) { 
-  var predicate = function(coord) {
-    !userData.hasOwnProperty(coord);
-  };
-  return eachCoord(userData, predicate);
-}
+exports.inRange = function(userData, coord) {
+    return coord === '' || +coord > 180 || +coord < 0;
+};
 
-
+exports.coordinatesExist = function(userData, coord) {
+    return userData.hasOwnProperty(coord);
+};
--- server/core/routeValidations.js
+++ server/core/routeValidations.js
@@ -1,11 +1,16 @@
 var validate = require('./basicValidations.js');
 
 exports.validateCoordinates = function(userData) {
-  return validate.coordinatesExist(userData) && 
-    validate.containsOnlyNumbers(userData) &&
-    validate.inRange(userData);
+  var predicates = [
+    validate.coordinatesExist,
+  validate.containsOnlyNumbers,
+  validate.inRange
+  ];
+
+  for (var i = 0;i < predicates.length;i++) {
+    if (!validate.eachCoord(userData, predicates[i])) {
+      return false;
+    }
+  }
+  return true;
 };
-
-var userData = {x: '33', y: '34', z: '32'};
-console.log(exports.validateCoordinates(userData));
-
