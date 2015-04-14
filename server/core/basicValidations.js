76068998315c08f4acec515b1a91e79a9e806008
Anthony Zotti <azotti1337@gmail.com>
2015-04-13 19:39:02 -0700

    Coordinate validations account for negative numbers

--- server/core/basicValidations.js
+++ server/core/basicValidations.js
@@ -1,6 +1,3 @@
-//basic validations are the building blocks for all other validations
-
-
 exports.eachCoord = function(userData, predicate) {
   var coordinatesToValidate = ['x', 'y', 'z'];
   for (var i = 0;i < coordinatesToValidate.length;i++) {
@@ -13,13 +10,13 @@ exports.eachCoord = function(userData, predicate) {
 };
 
 exports.containsOnlyNumbers = function(userData, coord) {
-    return !!/[^0-9]/.test(Math.floor(coord));
+  return !!/-[^0-9]/.test(Math.floor(coord));
 };
 
 exports.inRange = function(userData, coord) {
-    return coord === '' || +coord > 180 || +coord < 0;
+  return coord === '' || +coord > 180 || +coord < -180;
 };
 
 exports.coordinatesExist = function(userData, coord) {
-    return userData.hasOwnProperty(coord);
+  return userData.hasOwnProperty(coord);
 };
