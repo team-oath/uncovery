542f358d33dbd740fb40beb77ac758e32e269d13
scottdixon <scott@howrefreshing.com.au>
2015-04-14 15:18:11 -0700

    thumbnails/ abstracted thumbnail component

---
+++ client/components/Main/Thumbnails/index.js
@@ -0,0 +1,36 @@
+var React = require('react-native');
+
+var HOST = require('../../../config.js'); 
+
+var { Image, } = React;
+
+class Thumbnails extends React.Component {
+
+	constructor(props){
+		console.log(props);	
+		
+	}
+
+	render(){		
+		var window = require('Dimensions').get('window');
+		var uri = HOST + 'images?image=' + this.props.uri;
+		
+		var height = 100;
+		var resizeMode = Image.resizeMode.cover;
+
+		if (this.props.fullResolution){
+			//Square image
+			height = window.width;
+			resizeMode = Image.resizeMode.contain;
+		}
+
+		return (
+			<Image style={{height: height, resizeMode: resizeMode }} source={{uri: uri }} />
+		)
+		
+	}
+}
+
+module.exports = Thumbnails;
+
+// /thumb = '<thumbnails>'Thumbnails({ some: propertie });
