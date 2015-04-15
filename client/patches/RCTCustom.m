5c931bf46fdd56584ee27a92b081d0e75a3dc1bf
scottdixon <scott@howrefreshing.com.au>
2015-04-15 12:17:58 -0700

    post messages/ 500px wide images. Maintains aspect ratio.

--- client/patches/RCTCustom.m
+++ client/patches/RCTCustom.m
@@ -3,17 +3,19 @@
 // Objective-C
 #import "RCTBridgeModule.h"
 #import <AssetsLibrary/AssetsLibrary.h>
+#import "RCTCustom.h"
 #import <UIKit/UIKit.h>
+
 @interface ReadImageData : NSObject <RCTBridgeModule>
 @end
 
 @implementation ReadImageData
 
-RCT_EXPORT_MODULE(); 
+RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(processString:(NSString *)input callback:(RCTResponseSenderBlock)callback)
 {
- 
+  
   // Create NSURL from uri
   NSURL *url = [[NSURL alloc] initWithString:input];
   
@@ -26,20 +28,31 @@ RCT_EXPORT_METHOD(processString:(NSString *)input callback:(RCTResponseSenderBlo
     
     // Create an ALAssetRepresentation object using our asset
     // and turn it into a bitmap using the CGImageRef opaque type.
-    CGImageRef imageRef = [asset thumbnail];
+    CGImageRef imageRef = [[asset defaultRepresentation] fullResolutionImage];
+    
+    //Maintain aspect ratio, using maximum width of 500 and maximum height of 500.
+    CGFloat newWidth = 500 * (500.0f / CGImageGetWidth(imageRef));
+    CGFloat newHeight = 500* (500.0f / CGImageGetHeight(imageRef));
+    
+    //Draw WxH image on WxH canvas.
+    UIImage *img = [UIImage imageWithCGImage:imageRef];
+    UIGraphicsBeginImageContext(CGSizeMake(newWidth,newHeight));
+    [img drawInRect:CGRectMake(0, 0, newWidth, newHeight)];
+    UIImage *destImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
     // Create UIImageJPEGRepresentation from CGImageRef
-    NSData *imageData = UIImageJPEGRepresentation([UIImage imageWithCGImage:imageRef], 1);
+    NSData *imageData = UIImageJPEGRepresentation(destImage, 1);
     
     // Convert to base64 encoded string
     NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
     
+    //Trigger the callback with our data
     callback(@[base64Encoded]);
     
   } failureBlock:^(NSError *error) {
     NSLog(@"that didn't work %@", error);
   }];
-  
-
   
 }
 @end
