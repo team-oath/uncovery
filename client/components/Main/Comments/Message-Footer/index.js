841c010e724df4a076d5439928bd41ce33541a1f
Jonathan Huang <grokut@gmail.com>
2015-04-15 15:51:47 -0700

    feat/ heart toggling behavior

--- client/components/Main/Comments/Message-Footer/index.js
+++ client/components/Main/Comments/Message-Footer/index.js
@@ -8,14 +8,21 @@ var { View, Text, StyleSheet, TouchableOpacity, Image, } = React;
 var MessageFooter = React.createClass({
 
   getInitialState: function(){
-
     return {
       numHearts: this.props.numHearts,
       hasPressedHeart: this.props.hasPressedHeart,
     }
   },
 
+  componentWillReceiveProps: function(props){
+    this.setState({
+      numHearts: props.numHearts,
+      hasPressedHeart: props.hasPressedHeart,
+    })
+  },
+
   render: function(){
+    console.log(this.state.hasPressedHeart,'*************')
     return (
       <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
         <View>
@@ -26,12 +33,12 @@ var MessageFooter = React.createClass({
         <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
           <View>
             <Text style={styles.heartCounter}>
-              {this.state.numHearts}
+              {this.state.numHearts? this.state.numHearts : null}
             </Text>
           </View>
           <View style={{flex:1}}>
             <TouchableOpacity onPress={this._heartMessage}>
-            {this.state.heartPressed ? 
+            {this.state.hasPressedHeart ? 
               <Image
                 source={heartFilled}
                 style={{width:30, height:30}}
@@ -50,10 +57,20 @@ var MessageFooter = React.createClass({
   },
 
   _heartMessage: function(){
-    this.setState({
-      numHearts: this.state.numHearts+1,
-      hasPressedHeart: this.state.heartPressed ? false : true,
-    })
+
+    if ( this.state.hasPressedHeart ){
+      var decrement = this.state.numHearts - 1;
+      this.setState({
+        numHearts: decrement,
+        hasPressedHeart: false,
+      })
+    } else {
+      var increment = this.state.numHearts + 1;
+      this.setState({
+        numHearts: increment,
+        hasPressedHeart: true,
+      })
+    }
 
     fetch(HOST + 'upvote', {
       method: 'POST',
@@ -69,9 +86,6 @@ var MessageFooter = React.createClass({
     })
   }
 });
-
-// var heartImage = {uri: 'http://i.imgur.com/SXHb8nG.png?1'};
-// var heartFilled = {uri: 'http://i.imgur.com/6aglIdZ.png?1'};
 
 var heartImage = {uri: 'http://i.imgur.com/97rSbCf.png?1'};
 var heartFilled = {uri: 'http://i.imgur.com/SXHb8nG.png?1'};
