ff778a1cd7916f97e41f1daed42c9f0ef7a1521f
scottdixon <scott@howrefreshing.com.au>
2015-04-16 16:16:53 -0700

    camera roll/ moved styles to styles.js

--- client/components/CameraRollView.ios.js
+++ client/components/CameraRollView.ios.js
@@ -1,22 +1,8 @@
-/**
- * The examples provided by Facebook are for non-commercial testing and
- * evaluation purposes only.
- *
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * @providesModule CameraRollView
- * @flow
- */
 'use strict';
 
+var styles = require('../styles.js');
 var React = require('react-native');
+
 var {
   CameraRoll,
   Image,
@@ -186,26 +172,6 @@ var CameraRollView = React.createClass({
     this.setState(newState);
   },
 
-});
-
-var styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    flex: 1,
-  },
-  url: {
-    fontSize: 9,
-    marginBottom: 14,
-  },
-  image: {
-    margin: 4,
-  },
-  info: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-  },
 });
 
 module.exports = CameraRollView;
