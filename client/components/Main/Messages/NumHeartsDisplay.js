a4f60f105ce9bde6ae960f19a2d23d0535061886
Jonathan Huang <grokut@gmail.com>
2015-04-18 17:04:08 -0700

    feat/ websockets functionality for updating user's total score

--- client/components/Main/Messages/NumHeartsDisplay.js
+++ client/components/Main/Messages/NumHeartsDisplay.js
@@ -1,21 +1,40 @@
 
-var React = require('react-native')
+var React = require('react-native');
 
-var { View, Text, } = React;
+var io = require('./socket.io-client.js');
+
+var { View, Text, Image} = React;
 
 var NumHeartsDisplay = React.createClass({
 
-  render(){
+  getInitialState: function(){
+    return {
+      io: io('http://localhost:3000',{jsonp: false}),
+      numHearts: 15,
+    }
+  },
+
+  componentDidMount: function(){
+    var self = this;
+    this.state.io.on('upvote', function(){
+      var increment = self.state.numHearts + 1;
+      self.setState({numHearts: increment});
+    });
+  },
+
+  render: function(){
     return (
-      <View>
+      <View style={{marginLeft: 7, marginBottom: 5, alignItems: 'flex-end', justifyContent: 'space-between', flexDirection:'row',}}>
+        <Image
+          source={{uri: 'http://i.imgur.com/fMdIPV5.png'}}
+          style={{width: 25, height: 25, justifyContent: 'space-between'}}
+        />
         <Text style={{
           color:'white', 
           fontSize: 20, 
-          marginLeft: 15, 
-          marginBottom: 5, 
-          fontFamily: 'Avenir'
+          fontFamily: 'Avenir',
         }}>
-          15
+        {this.state.numHearts}
         </Text>
       </View>
     );
