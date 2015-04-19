bf17e4e73bebe50179a81c7563b1892cee48fdca
R. Tyler Julian <trjulian@gmail.com>
2015-04-19 18:53:43 -0400

    Add before and after hooks for socket testing

--- specs/sockets.js
+++ specs/sockets.js
@@ -1,9 +1,39 @@
 var expect = require('chai').expect;
 var io = require('socket.io-client');
+var models = require('../server/db/models');
 var localServerUri = 'http://127.0.0.1:3000/';
+var testUser = {userToken: 'ABCDEFG'};
+var testMsg = {x: 100, y: 100, z: 100, message: 'hello database!', userToken: testUser.userToken};
+var testComment = {x: 101, y: 101, z: 101, commentString: 'hello database comment!', userToken: testUser.userToken};
+var testVote = {userToken: testUser.userToken};
 var connection;
 
 describe('Socket Server', function() {
+
+  before(function(done) {
+    models.createUser(testUser.userToken)
+    .then(function(data) {
+      testVote.messageId = data.insertId;
+      done();
+    });
+  });
+
+  after(function(done) {
+    models.deleteRow('marks', ['x', testMsg.x])
+    .then(function() {
+      return models.deleteRow('marks', ['x', testComment.x]);
+    }).then(function() {
+      return models.deleteRow('votes', ['userToken', testVote.userToken]);
+    }).then(function() {
+      return models.deleteRow('comments', ['commentString', testComment.commentString]);
+    }).then(function() {
+      return models.deleteRow('messages', ['messageString', testMsg.message]);
+    }).then(function() {
+      return models.deleteRow('users', ['token', testUser.userToken]);
+    }).then(function() {
+      done();
+    });
+  });
 
   it('should receive a new socket connection', function(done) {
     connection = io(localServerUri);
@@ -13,10 +43,10 @@ describe('Socket Server', function() {
   });
 
   it('should authenticate the connection', function(done) {
-    var userObject = {userToken: 'ABCDEFG'};
-    connection.emit('init', userObject);
+    connection.emit('init', testUser);
     connection.on('init', function() {
       done();
     });
   });
+
 });
